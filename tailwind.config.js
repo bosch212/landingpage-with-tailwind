13119b759a29b2217efc4bf9eb317675beffce94
bosch212 <nasrulaytam@gmail.com>
2022-01-06 23:46:09 +0700

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            orange: "#FF7143",
+            "orange-dark": "#9F3919",
+            darkblue: "#5454D4",
+            "darkblue-200": "#3f3f9f",
+            black: "#19191B",
+            gray: "#696871",
+            "gray-light": "#E7E7E7",
+            white: "#fff",
+            "white-light": "#F7F7FB",
+            "white-lighter": "#F8F8F8",
+            transparent: "transparent"
+        },
+        fontSize: {
+            'xs': '.75rem',
+            'sm': '.875rem',
+             'base': '1rem',
+             'lg': '1.125rem',
+             'xl': '1.25rem',
+             '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+             '5xl': '3rem',
+             '6xl': '3.5rem',
+            '7xl': '5rem',
+           },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
+  
